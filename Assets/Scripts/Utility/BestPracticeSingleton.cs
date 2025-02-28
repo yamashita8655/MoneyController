4cb19f5523fba41650309975566833af832c5a5a
yamashita <wisdomword-justice@hotmail.co.jp>
2025-02-28 13:16:12 +0900

    シングルトンでオブジェクト検索する方法が変わっていたので対応

--- Assets/Scripts/Utility/BestPracticeSingleton.cs
+++ Assets/Scripts/Utility/BestPracticeSingleton.cs
@@ -11,8 +11,8 @@ public class BestPracticeSingleton<T> : MonoBehaviour where T : MonoBehaviour {
 	//Returns the instance of this singleton. 
 	public static T Instance { 
 		get {
-			if(instance == null) { 
-				instance = (T) FindObjectOfType(typeof(T)); 
+			if(instance == null) {
+                instance = FindFirstObjectByType<T>(FindObjectsInactive.Include);
 	
 				if (instance == null) { 
 					Debug.LogError("An instance of " + typeof(T) +  " is needed in the scene, but there is none."); 
